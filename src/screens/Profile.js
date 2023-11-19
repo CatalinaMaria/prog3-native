fe9d794a43f88688448a24d86581a9e588a0cb02
CatalinaMaria <cdearzubiagamenghi@udesa.edu.ar>
2023-11-19 20:56:21 -0300

    css y modal

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Text, View, TouchableOpacity, StyleSheet, FlatList, Image } from 'react-native';
+import React, { Component, useState } from 'react';
+import { Text, View, TouchableOpacity, StyleSheet, FlatList, Image, Modal, Pressable, Alert } from 'react-native';
 import { auth, db } from '../firebase/config';
 import Post from '../components/Post';
 
@@ -9,6 +9,8 @@ export default class Profile extends Component {
     this.state = {
       usuarios: [],
       posteos: [],
+      selectedPostId: null,
+      modalVisible: false,
     };
   }
 
@@ -46,23 +48,42 @@ export default class Profile extends Component {
       });
   }
 
-  borrarPost(postId) {
-    db.collection('posts')
-      .doc(postId)
-      .delete()
-      .then(() => {
-        console.log('Post eliminado correctamente');
-      })
-      .catch((error) => {
-        console.error('Error al eliminar el post:', error);
+  confirmarBorrado = async (postId) => {
+    try {
+      await db.collection('posts').doc(postId).delete();
+      console.log('Post eliminado correctamente');
+
+      // Actualizar el estado después de borrar el posteo
+      const updatedPosteos = this.state.posteos.filter(post => post.id !== postId);
+      this.setState({
+        posteos: updatedPosteos,
+        modalVisible: false,
       });
+    } catch (error) {
+      console.error('Error al eliminar el post:', error);
+    }
+  }
+
+  borrarPost = (postId) => {
+    // Mostrar el modal de confirmación
+    this.setState({
+      selectedPostId: postId,
+      modalVisible: true,
+    });
+  }
+
+  closeModal = () => {
+    // Cerrar el modal sin borrar el posteo
+    this.setState({
+      selectedPostId: null,
+      modalVisible: false,
+    });
   }
 
   logout = () => {
     auth.signOut();
     this.props.navigation.navigate('Register');
   }
-  
 
   handleChangePassword = () => {
     this.props.navigation.navigate('ChangePasswordScreen');
@@ -124,6 +145,35 @@ export default class Profile extends Component {
             <Text>Cerrar sesión</Text>
           </TouchableOpacity>
         </View>
+
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            this.closeModal();
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>¿Estás seguro de que quieres borrar este posteo?</Text>
+              <View style={styles.modalButtons}>
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => this.closeModal()}
+                >
+                  <Text style={styles.textStyle}>Cancelar</Text>
+                </Pressable>
+                <Pressable
+                  style={[styles.button, styles.buttonConfirm]}
+                  onPress={() => this.confirmarBorrado(this.state.selectedPostId)}
+                >
+                  <Text style={styles.textStyle}>Borrar</Text>
+                </Pressable>
+              </View>
+            </View>
+          </View>
+        </Modal>
       </View>
     );
   }
@@ -184,11 +234,59 @@ const styles = StyleSheet.create({
     marginBottom: 15,
   },
   editProfileButton: {
-    color: 'blue',
-    textDecorationLine: 'underline',
+    color: 'black',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textDecorationLine: 'none',
     marginTop: 10,
   },
   changePasswordButton: {
     marginTop: 10,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  modalButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+  },
+  button: {
+    borderRadius: 5,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  buttonConfirm: {
+    backgroundColor: '#FF6347',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
